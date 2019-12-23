8dfb29976595d7a3a62d36923d9f18ec82423481
Leigh Halliday <leighhalliday@gmail.com>
2019-12-23 15:30:00 -0500

    Remote data

--- src/App.js
+++ src/App.js
@@ -1,51 +1,53 @@
 import React from "react";
 import { Map, Marker, Popup, TileLayer } from "react-leaflet";
 import { Icon } from "leaflet";
-import * as parkData from "./data/skateboard-parks.json";
+import useSwr from "swr";
 import "./app.css";
 
-export const icon = new Icon({
-  iconUrl: "/skateboarding.svg",
+const fetcher = (...args) => fetch(...args).then(response => response.json());
+
+const icon = new Icon({
+  iconUrl: "/handcuffs.svg",
   iconSize: [25, 25]
 });
 
 export default function App() {
-  const [activePark, setActivePark] = React.useState(null);
+  const url =
+    "https://data.police.uk/api/crimes-street/all-crime?lat=52.629729&lng=-1.131592&date=2019-10";
+  const { data, error } = useSwr(url, fetcher);
+  const crimes = data && !error ? data.slice(0, 100) : [];
+  const [activeCrime, setActiveCrime] = React.useState(null);
 
   return (
-    <Map center={[45.4, -75.7]} zoom={12}>
+    <Map center={[52.6376, -1.135171]} zoom={12}>
       <TileLayer
         url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
         attribution='&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
       />
 
-      {parkData.features.map(park => (
+      {crimes.map(crime => (
         <Marker
-          key={park.properties.PARK_ID}
-          position={[
-            park.geometry.coordinates[1],
-            park.geometry.coordinates[0]
-          ]}
-          onClick={() => {
-            setActivePark(park);
-          }}
+          key={crime.id}
+          position={[crime.location.latitude, crime.location.longitude]}
           icon={icon}
+          onClick={() => {
+            setActiveCrime(crime);
+          }}
         />
       ))}
 
-      {activePark && (
+      {activeCrime && (
         <Popup
           position={[
-            activePark.geometry.coordinates[1],
-            activePark.geometry.coordinates[0]
+            activeCrime.location.latitude,
+            activeCrime.location.longitude
           ]}
           onClose={() => {
-            setActivePark(null);
+            setActiveCrime(null);
           }}
         >
           <div>
-            <h2>{activePark.properties.NAME}</h2>
-            <p>{activePark.properties.DESCRIPTIO}</p>
+            <h2>{activeCrime.category}</h2>
           </div>
         </Popup>
       )}
